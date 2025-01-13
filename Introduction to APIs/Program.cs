f64820b1b8ebe1c3bbf4d7692fe4e5433bd00a66
Moaad Amer <moaad02@gmail.com>
2025-01-13 22:47:58 +0200

    C# Asp.net web api - playing around

---
+++ Introduction to APIs/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+var todos = new List<Todo>();
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => t.Id == id);
+    return targetTodo is null ?
+    TypedResults.NotFound() :
+    TypedResults.Ok(targetTodo);
+});
+
+app.MapGet("/todos", ()=> todos);
+
+app.Run();
+
+
+
+
+
+public record Todo(int Id, string Name, DateTime DueDate, bool isCompleted);
