8246dc6b903884347991d8b45ebd5af128edb55c
Moaad Amer <moaad02@gmail.com>
2025-01-23 15:55:10 +0200

    create a errors filter on todo post

--- Introduction to APIs/Program.cs
+++ Introduction to APIs/Program.cs
@@ -17,13 +17,34 @@ app.Use(async (context, next) =>
     Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.Now}] Finshed.");
 });
 
+
+
 var todos = new List<Todo>();
 
 app.MapPost("/todos", (Todo task) =>
 {
     todos.Add(task);
     return TypedResults.Created("/todos/{id}", task);
-});
+})
+.AddEndpointFilter(async (context, next) =>
+    {
+        var taskArgument = context.GetArgument<Todo>(0);
+        var errors = new Dictionary<string, string[]>();
+        if (taskArgument.DueDate < DateTime.UtcNow)
+        {
+            errors.Add(nameof(taskArgument.DueDate), ["Cannot have due date in the past."]);
+        }
+        if (taskArgument.isCompleted)
+        {
+            errors.Add(nameof(taskArgument.isCompleted), ["Cannot add a completed todo."]);
+        }
+        if (errors.Count > 0)
+        {
+            return Results.ValidationProblem(errors);
+        }
+
+        return await next(context);
+    });
 
 app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
 {
