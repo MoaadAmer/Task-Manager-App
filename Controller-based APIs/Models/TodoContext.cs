a57ca6d71865a1f5cc96ddc7441628d13c2db654
Moaad Amer <moaad02@gmail.com>
2025-01-24 17:08:58 +0200

    Create basic Todo CRUD API

---
+++ Controller-based APIs/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Controller_based_APIs.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+        public DbSet<TodoItem>? TodoItems { get; set; } = null;
+    }
+}
